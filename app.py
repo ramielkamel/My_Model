cd4bb4ed38ceab8502b6ca63c4f375c4fa517699
ramielkamel <94676131+ramielkamel@users.noreply.github.com>
2021-12-12 20:54:54 +0100

    Add files via upload

---
+++ app.py
@@ -0,0 +1,45 @@
+from flask import Flask, render_template, request, redirect
+from gevent.pywsgi import WSGIServer
+
+import pandas as pd
+import customer
+
+
+app = Flask(__name__)
+
+
+#@app.route("/", methods = ['GET'])
+#def hello_world():
+#   return render_template('index.html')
+
+
+# @app.route("/predict", methods = ['POST'])
+# def predict():
+#     res = request.form
+    
+#     data = pd.DataFrame.from_dict(res, orient='index').T
+#     print(data)
+#     # return data
+#     return res, 200 
+
+@app.route("/predict", methods = ['POST'])
+def predict():
+    if request.method == 'POST':
+        res = request.form
+        #data = pd.DataFrame.from_dict(res, orient='index').T
+        #print(data)
+        x = customer.get_data(res)
+        if x[0] == 0:
+            result = "customer will not leave"
+        else:
+            result = "customer will leave"
+        return render_template('result.html', result = result)
+
+@app.route("/", methods = ['GET', 'POST'])
+def hello_world():
+    return render_template('index.html')
+
+if __name__ == "__main__":
+    app.run(debug=True)
+    http_server = WSGIServer(('', 5000), app)
+    http_server.serve_forever()
